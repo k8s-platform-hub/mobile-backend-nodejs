d3c8cf8cc2ab2d04649ba01ac945058c6206a237
wawhal <rishichandra.wawhal@gmail.com>
2018-02-16 14:44:17 +0530

    DeploymentPush

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 const app = express();
-const server = http.createServer(app);
+const server = http.Server(app);
 
 const bodyParser = require('body-parser');
 
