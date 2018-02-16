de762078fbe3034aeb159ed03f5a93e161fb93e1
wawhal <rishichandra.wawhal@gmail.com>
2018-02-16 14:40:05 +0530

    DeploymentPush

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -18,7 +18,8 @@ const io = socketClient(server);
 app.use(serverRoutes);
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+
+  res.header("Access-Control-Allow-Origin", req.get("origin"));
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
